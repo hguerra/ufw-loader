0bca2fbb9cd8dc5beb56bc6a3c75a1a17b9ad5bf
Hunter Kirk <whk@whk.io>
2018-01-26 13:45:44 -0600

    Added fetch script for China, Russia, Netherlands, and Ukraine.

---
+++ fetch.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Ukraine
+wget -O ua.txt http://www.ipdeny.com/ipblocks/data/aggregated/ua-aggregated.zone
+# China
+wget -O cn.txt http://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone
+# Russia
+wget -O ru.txt http://www.ipdeny.com/ipblocks/data/aggregated/ru-aggregated.zone
+# Netherlands
+wget -O nl.txt http://www.ipdeny.com/ipblocks/data/aggregated/nl-aggregated.zone
