5af372669b0acdd75a1278c5399898d9a4b5511e
Hunter Kirk <whk@whk.io>
2018-01-26 13:51:20 -0600

    Uploaded unload.sh to unload all defined lists.

---
+++ unload.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+count=1
+countries=( ua.txt cn.txt ru.txt nl.txt )
+
+for i in "${countries[@]}"
+do
+  file=$i
+  nblines=$(grep -c '' $file)
+
+  while read p; do
+    echo $count of $nblines
+    sudo ufw delete deny from $p to any
+    let "count+=1"
+  done <$file
+done
+
+
